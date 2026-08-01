edc47f900229519e5a7c88e28ee03c5086bb7193
Raicley Contadini Sampaio <81638286+raicleycs@users.noreply.github.com>
2026-07-31 22:19:49 -0300

    Delete navegacao.js

--- navegacao.js
+++
@@ -1,7 +0,0 @@
-export const navegacao = [
-    {link:'Dashboard', roteador:'#/dashboard'},
-    {link:'Contato', roteador:'#/contato'},
-    {link:'Clientes', roteador:'#/clientes'},
-    {link:'Relatórios', roteador:'#/relatorios'},
-    {link:'Configurações', roteador:'#/configuracao'}
-];
